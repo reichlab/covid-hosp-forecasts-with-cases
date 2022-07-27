3a5555e8f04b2b154e29f7f7adcee87caa1af270
Evan Ray <elray@umass.edu>
2022-07-27 15:20:54 -0400

    partial progress on managing dates and zeros

--- code/fit-sarix-models/run-all-models-one-state-date-case-type.py
+++ code/fit-sarix-models/run-all-models-one-state-date-case-type.py
@@ -18,9 +18,96 @@ def expand_grid(data_dict):
     return pd.DataFrame.from_records(rows, columns=data_dict.keys())
 
 
-def load_data(as_of=None, end_day="2021-07-01", case_type='report', case_timing='final', state='ma'):
+def count_zeros(values):
+    '''Count the number of entries in values that are zero'''
+    return sum(values == 0.)
+
+
+def prop_zeros(values):
+    '''Calculate the proportion of entries in values that are zero'''
+    return sum(values == 0.) / len(values)
+
+
+def clean_outliers(values):
+    result = values.copy()
+    
+    # set all trailing zeros to nan; will be filled with the last nonzero value later
+    num_trailing_zeros = len(result.values) - np.max(np.nonzero(values.values)) - 1
+    if num_trailing_zeros > 0:
+        result[-num_trailing_zeros:] = np.nan
+    
+    # count number of zero values in a centered rolling 7 day window
+    # fill in zero counts at end with last value
+    zero_counts = result.rolling(7, center = True).apply(count_zeros)
+    zero_counts = zero_counts.fillna(method='ffill')
+    
+    zero_props = result.rolling(7, center = True).apply(prop_zeros)
+    zero_props = zero_props.fillna(method='ffill')
+    
+    # if more than 2 days of zeros in rolling window, replace with rolling mean
+    inds_to_replace = (zero_counts > 2) & (zero_props < 1.0)
+    replace_vals = result.rolling(7, center = True).mean()
+    replace_vals = replace_vals.fillna(method='ffill')
+    result[inds_to_replace] = replace_vals[inds_to_replace]
+    
+    # if 1 or 2 days of zeros in rolling window, set to nan
+    inds_to_replace = (((zero_counts > 0) & (zero_counts <= 2)) | zero_props == 1.0) & (result == 0.)
+    result[inds_to_replace] = np.nan
+    
+    # detect outliers as rolling median +/- 3 IQR, set to nan
+    rm = result.rolling(15, center = True, min_periods=1).median()
+    resids = result - rm
+    riqr = resids.rolling(15, center = True, min_periods=1).quantile(0.75) - \
+        resids.rolling(15, center = True, min_periods=1).quantile(0.25)
+    lower = (rm - 3 * riqr).fillna(method = 'ffill')
+    upper = (rm + 3 * riqr).fillna(method = 'ffill')
+    is_outlr = (result < lower) | (result > upper)
+    result[is_outlr] = np.nan
+    
+    # linearly interpolate nan's (corresponding to zeros and detected outliers)
+    result.interpolate(inplace=True, limit_direction='both')
+    
+    return result
+
+
+def redist_zero_runs(values, include_leading = False):
+    # adapted from https://stackoverflow.com/questions/1066758/find-length-of-sequences-of-identical-values-in-a-numpy-array-run-length-encodi
+    values = values.copy()
+    lead_adj = -1 * include_leading
+    n = len(values)
+    zero_ind = (values == 0.)
+    y = zero_ind[1:] != zero_ind[:-1]   # pairwise unequal
+    i = np.append(np.where(y), n - 1)   # must include last element posi
+    z = np.diff(np.append(-1, i))       # run lengths
+    p = np.cumsum(np.append(0, z))[:-1] # positions
+    if values[0] != 0.:
+        i = i[1:]
+        z = z[1:]
+        p = p[1:]
+    
+    # following line throws an error if last value is a zero
+    ranges = p.reshape((len(p) // 2, 2))
+    for j in range(ranges.shape[0]):
+        values[(ranges[j, 0] + lead_adj):(ranges[j,1]+1)] = np.mean(values[(ranges[j, 0] + lead_adj):(ranges[j,1]+1)])
+    
+    # set trailing zeros to nan and then fill with the last nonzero value
+    num_trailing_zeros = len(values) - np.max(np.nonzero(values)) - 1
+    if num_trailing_zeros > 0:
+        values[-num_trailing_zeros:] = np.nan
+    
+    values.interpolate(inplace=True, limit_direction='both')
+    
+    return values
+
+
+def load_data(as_of=None,
+              end_day="2021-07-01",
+              case_type='report',
+              state='ma',
+              case_source = 'jhu-csse',
+              outlier_correction = 'none'):
     """
-    Load data for MA cases and hosps from covidcast
+    Load data for cases and hosps from covidcast or state DPH records
     Parameters
     ----------
     as_of: string of date in YYYY-MM-DD format.
@@ -29,21 +116,28 @@ def load_data(as_of=None, end_day="2021-07-01", case_type='report', case_timing=
             Default to "2021-07-01"
     case_type: string of recording method for Covid-19 cases
             Default to 'report'
-    case_timing: string to indiciate versioning.
-            Default to 'final'
     state: string of a 2-letter state abbreviation.
             Default to 'ma'
+    case_source: string specifying data source for case data
+            Default to 'jhu-csse', other option is 'dph' for state dph records
+    outlier_correction: string specifying method for outlier correction.
+            Correction is done only for outliers in report date cases.
+            Default to 'none', in which case the original data are returned.
+            'interp_zeros' linearly interpolates zeros.
+            'interp_zeros_outliers' linearly interpolates zeros and outliers
+            detected as being more than 3 IQR's away from a rolling median
+            'redist_zero_runs' replaces runs of zeros and the adjacent days with
+            the mean over that time span.
+    
     Returns
     -------
     df: data frame
             It has columns location, date, inc_hosp, population and rate.
             It is sorted by location and date columns in ascending order.
     """
-    # override as_of = None to use the same as_of as is used for cases
+    # override as_of = None to use the as_of for "finalized data"
     if as_of is None:
-        hosp_as_of = '2022-04-29'
-    else:
-        hosp_as_of = as_of
+        as_of = '2022-04-29'
     
     # load hospitalizations
     hosp_df = covidcast.signal(data_source="hhs",
@@ -52,47 +146,74 @@ def load_data(as_of=None, end_day="2021-07-01", case_type='report', case_timing=
                                end_day=date.fromisoformat(end_day),
                                geo_type="state",
                                geo_values=state,
-                               as_of=date.fromisoformat(hosp_as_of))
+                               as_of=date.fromisoformat(as_of))
     hosp_df = hosp_df[["geo_value", "time_value", "value"]]
     hosp_df.columns = ["location", "date", "hosp"]
     
     # load cases
     if case_type == 'report':
-        if case_timing == 'final':
-            case_as_of = '2022-04-29'
-        else:
-            case_as_of = as_of
+        # if case_timing == 'final':
+        #     case_as_of = '2022-04-29'
+        # else:
+        #     case_as_of = as_of
         
-        case_df = covidcast.signal(data_source="jhu-csse",
-                                   signal="confirmed_incidence_num",
-                                   start_day=date.fromisoformat("2020-10-01"),
-                                   end_day=date.fromisoformat(end_day),
-                                   geo_type="state",
-                                   geo_values=state,
-                                   as_of=date.fromisoformat(case_as_of))
-        case_df = case_df[["geo_value", "time_value", "value"]]
-        case_df.columns = ["location", "date", "case"]
+        if case_source == 'jhu-csse':
+            case_df = covidcast.signal(data_source="jhu-csse",
+                                    signal="confirmed_incidence_num",
+                                    start_day=date.fromisoformat("2020-10-01"),
+                                    end_day=date.fromisoformat(end_day),
+                                    geo_type="state",
+                                    geo_values=state,
+                                    as_of=date.fromisoformat(as_of))
+            case_df = case_df[["geo_value", "time_value", "value"]]
+            case_df.columns = ["location", "date", "case"]
+        elif case_source == 'dph':
+            if state == 'ca':
+                # if case_timing == 'final':
+                #     case_as_of = '2022-04-29'
+                # else:
+                #     raise ValueError("For case_source 'dph', only case_timing 'final' is supported")
+                
+                case_df_path = 'csv-data/CA-DPH-reportdate-covid-' + as_of + '.csv'
+                case_df = pd.read_csv(case_df_path)
+                case_df['location'] = state
+                case_df = case_df[['location', 'report_date', 'new_positive']]
+                case_df.columns = ['location', 'date', 'case']
+                
+                case_df = case_df[(case_df.date >= '2020-10-01')
+                                & (case_df.date <= end_day)]
+                case_df.date = pd.to_datetime(case_df.date)
+                
+            else:
+                raise ValueError("For case_source 'dph', only state 'ca' is supported")
         
         # quick fix to zero values; replace with nan and then interpolate missing
-        zero_inds = np.where(case_df.case == 0)
-        case_df['case'].iloc[zero_inds] = np.nan
-        case_df.interpolate(inplace=True)
+        if outlier_correction == 'interp_zeros':
+            zero_inds = np.where(case_df.case == 0)
+            case_df['case'].iloc[zero_inds] = np.nan
+            case_df['case'] = case_df['case'].interpolate()
+        elif outlier_correction == 'interp_zeros_outliers':
+            case_df.case = clean_outliers(case_df.case)
+        elif outlier_correction == 'redist_zero_runs':
+            case_df.case = redist_zero_runs(case_df.case.values, include_leading=True)
+        elif outlier_correction != 'none':
+            raise ValueError("outlier_correction must be 'none', 'interp_zeros', 'interp_zeros_outliers', or 'redist_zero_runs'")
     else:
-        csv_files = os.listdir('csv-data')
-        as_ofs = [f[21:-4] for f in csv_files]
-        if case_timing == 'final':
-            if state == 'ma':
-                case_as_of = '2022-04-28'
-            elif state == 'ca':
-                case_as_of = '2022-04-29'
-        else:
-            subset_as_ofs = [ao for ao in as_ofs if ao <= as_of]
-            case_as_of = max(subset_as_ofs)
+        # csv_files = os.listdir('csv-data')
+        # as_ofs = [f[21:-4] for f in csv_files]
+        # if case_timing == 'final':
+        #     if state == 'ma':
+        #         case_as_of = '2022-04-28'
+        #     elif state == 'ca':
+        #         case_as_of = '2022-04-29'
+        # else:
+        #     subset_as_ofs = [ao for ao in as_ofs if ao <= as_of]
+        #     case_as_of = max(subset_as_ofs)
         
         if state == 'ma':
-            case_df_path = 'csv-data/MA-DPH-csvdata-covid-' + case_as_of + '.csv'
+            case_df_path = 'csv-data/MA-DPH-csvdata-covid-' + as_of + '.csv'
         elif state == 'ca':
-            case_df_path = 'csv-data/CA-DPH-testdate-covid-' + case_as_of + '.csv'
+            case_df_path = 'csv-data/CA-DPH-testdate-covid-' + as_of + '.csv'
         
         case_df = pd.read_csv(case_df_path)
         case_df['location'] = state
@@ -113,9 +234,11 @@ def load_data(as_of=None, end_day="2021-07-01", case_type='report', case_timing=
     df[['case', 'hosp']] = df[['case', 'hosp']].astype('float64')
     
     # drop missing values; assumed to be trailing (dangerous?)
+    print(df)
     df = df.dropna()
     
     return df
+
 
 
 def construct_forecast_df(location, forecast_date, pred_qs, q_levels, base_target):
@@ -195,14 +318,18 @@ if __name__ == "__main__":
     parser.add_argument("--forecast_date", nargs="?",
                         default='2020-12-07', type=str)
     parser.add_argument("--case_type", nargs="?", default='test', type=str)
-    parser.add_argument("--case_timing", nargs="?", default='final', type=str)
+    parser.add_argument("--case_source", nargs="?", default='jhu-csse', type=str)
+    parser.add_argument("--data_timing", nargs="?", default='final', type=str)
+    parser.add_argument("--outlier_correction", nargs="?", default='final', type=str)
     parser.add_argument("--transform", nargs="?",
                         default='fourth_rt', type=str)
     args = parser.parse_args()
     state = args.state
     forecast_date = args.forecast_date
     case_type = args.case_type
-    case_timing = args.case_timing
+    case_source = args.case_source
+    data_timing = args.data_timing
+    outlier_correction = args.outlier_correction
     transform = args.transform
     # state = 'ma'
     # state = 'ca'
@@ -286,7 +413,7 @@ if __name__ == "__main__":
 
     # keep only variations without a model fit file
     model_names = [build_model_name(case_type,
-                                    case_timing,
+                                    data_timing,
                                     sari_variations.smooth_case.values[i],
                                     sari_variations.p.values[i],
                                     sari_variations.d.values[i],
@@ -302,11 +429,16 @@ if __name__ == "__main__":
     # only proceed if there are models to fit
     if sari_variations.shape[0] > 0:
         # load data
-        # should end_day be forecast_date - 1?
-        data = load_data(as_of=forecast_date, end_day=forecast_date,
-                         case_type=case_type, case_timing=case_timing,
-                         state=state)
-
+        # as_of is set to the value of the finalized data date
+        if data_timing == 'final':
+            as_of = '' # TODO
+        else:
+            as_of = forecast_date
+        
+        data = load_data(as_of=as_of, end_day=forecast_date,
+                         case_type=case_type, case_source=case_source,
+                         state=state, outlier_correction=outlier_correction)
+        
         # data transform
         if transform == "sqrt":
             data.case[data.case <= 0] = 1.0
